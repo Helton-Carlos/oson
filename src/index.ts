b2127ec3ddf5e553fcde9311d0b619db95fe6189
Helton Brito <heltoncarlospa@hotmail.com>
2025-09-28 09:45:46 -0300

    feat: install express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hey')
+import * as express from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
